d0b5661999b3bb13a749dade64825c6172839c46
Kin Ueng <github@kinueng.com>
2019-10-04 16:37:03 -0500

    refactor: Move inline code to files
    - Move the javascript and stylesheet code from being inline in the code,
    to dedicated files.  Use node's filesystem read to get the code from the
    new files.

---
+++ openshift/appLauncher.js
@@ -0,0 +1,8 @@
+(function() {
+    window.OPENSHIFT_CONSTANTS.APP_LAUNCHER_NAVIGATION = [{
+        title: "kAppNav",                            // The text label
+        iconClass: "icon-appnav",                    // The icon you want to appearl
+        href: "${url}",                              // Where to go when this item is clicked
+        tooltip: "Kubernetes Application Navigator"  // Optional tooltip to display on hover
+    }]
+}())
---
+++ openshift/featuredApp.js
@@ -0,0 +1,8 @@
+(function() {
+    window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS = [{
+        title: "kAppNav",                           // The text label
+        icon: "icon-kappnav-feature",               // The icon you want to appear
+        url: "${url}",      //  Where to go when this item is clicked
+        description: "Kubernetes Application Navigator"      // Short description
+    }]
+}())

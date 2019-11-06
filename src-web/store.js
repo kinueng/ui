036d622bc618c4e491a82aa715c9bbf4b0773b26
Kin Ueng <github@kinueng.com>
2019-11-06 09:19:18 -0600

    Remove redux devtools from product code

--- src-web/store.js
+++ src-web/store.js
@@ -1,14 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import BaseReducer from './reducers/BaseServiceReducer';
 
 const reducer = combineReducers({
   baseInfo: BaseReducer,
 })
 
-export default createStore(reducer,
-  composeWithDevTools(
-    applyMiddleware(thunk)
-  )
+export default createStore(
+  reducer,
+  applyMiddleware(thunk)
 );

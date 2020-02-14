a87a953d4a10205860d95a0a3518667268ada3f5
Kin Ueng <github@kinueng.com>
2020-02-13 22:42:51 -0600

    feat(notification): Redesign completed jobs alerts
    MomentJS Warnings
    - fix MomentJS deprecation warnings with .add(). The parameters need to
    be switched.
    
    Redux/Configurable polling interval time
    - redux was integrated so that the polling interval can be controlled by
     the kappnav-config ConfigMap, specifically the
    'ui-completed-jobs-polling-interval-in-ms' property.  An example would
    be:
    ui-completed-jobs-polling-interval-in-ms: '5000'
    
    Polling behavior changes
    - Changed the polling to always make an initial API call on browser load
     or refresh.  Before, a polling would increment the time scope for
    completed jobs.  The time scope is no longer based on this idea.  The
    time scope for completed jobs is now based on the last seen completed
    job.  This means that each poll no longer increments the time scope and
    the polling is functionally harmless (except performance).
    
    Timestamps
    - OpenShift 4.3 stores timestamps in YYYY-MM-DDTHH:MM:ssZ format.  When
    this format is translated into the JSON from the API, the format becomes
     YYYY-MM-HH-ss.SSSZ.  Note the SSS is 000 because OpenShift only stores
    the timestamps to the seconds.

--- src-web/components/kappnav/common/NotificationHandler.js
+++ src-web/components/kappnav/common/NotificationHandler.js
@@ -16,22 +16,43 @@
 *
 *****************************************************************/
 
-// This handler will keep polling for new events and keep track of what should be shown
-// to the user
+/** 
+ * This handler will keep polling for new events and keep track of what should be shown
+ * to the user.
+ */
+
 
 import React from 'react'
 import moment from 'moment'
 import Notification from './Notification'
+import { connect } from 'react-redux'
 
-const api_polling_intervals_milliseconds = 30000 // ms
-const localStorage_key = 'timestamp-of-last-api-poll'
+const default_api_polling_intervals_milliseconds = 30000 // ms
 
-export default class NotificationHandler extends React.PureComponent {
+// Browser local storage keys - start
+const localStorage_key_polling_time = 'timestamp-of-last-api-poll'
+const localStorage_key_last_seen_completed_job_timestamp = 'timestamp-of-last-seen-completed-job'
+// Browser local storage keys - end
+
+class NotificationHandler extends React.PureComponent {
     
     constructor(props) {
         super(props)
         this.state = {
             completed_commands: []
+        }
+    }
+
+    /**
+     * Get the polling interval from the Kappav configuration
+     */
+    getPollingIntervalFromConfig() {
+        const interval = this.props.baseInfo.appNavConfigMap.completedJobsPollingInterval
+        if(! interval) {
+            // return the default
+            return default_api_polling_intervals_milliseconds 
+        } else {
+            return parseInt(interval, 10); // 10 is for mathmatical base number
         }
     }
 
@@ -55,10 +76,12 @@ export default class NotificationHandler extends React.PureComponent {
         // Do initial fetch of completed jobs
         this.getCompletedJobs()
 
+        const pollingInterval = this.getPollingIntervalFromConfig()
+
         // Setup the continuous API polling
         this.timer = setInterval(
                 () => this.getCompletedJobs(), 
-                api_polling_intervals_milliseconds);
+                pollingInterval);
     }
     
     componentWillUnmount() {
@@ -73,34 +96,62 @@ export default class NotificationHandler extends React.PureComponent {
     setLastAPICheckTime() {
         const store = window.localStorage
         const currentTime = moment()
-        store.setItem(localStorage_key, currentTime)
+        store.setItem(localStorage_key_polling_time, currentTime)
     }
 
     /**
      * Return the last timestamp the API was polled
      */
     getLastAPICheckTime() {
-        return window.localStorage.getItem(localStorage_key)
+        return window.localStorage.getItem(localStorage_key_polling_time)
     }
 
     /**
-     * Poll for completed jobs only if the last time polled has reached or exceeded 
-     * a set amount of time passed relative to current time.
+     * Record the most recent completed job's completion timestamp
+     * 
+     * @param {*} jobs - array containing jsons that represent each job
      */
-    getCompletedJobs() {
-        if(! this.isItTimeToPollAPI()) {
-            // Too early to poll API, already polled recently
-            return
+    setMostRecentJobCompletionTime(jobs) {
+        if(!jobs || !jobs.commands || jobs.commands.length === 0) {
+            // No recently completed jobs, do nothing
+            return undefined
         }
 
-        // API mandates `?time=` format to be:
-        // yyyy-MM-dd'T'HH:mm:sss (without the ' around the T)
-        const timeStampFromThirtySecondsAgo = 
-            this.getPastTimeStampFrom(api_polling_intervals_milliseconds).format('YYYY-MM-DDTHH:mm:ss')
+        let timestamps = []
+        jobs.commands.forEach(job => {
+            // Extract all the completion timestamps into an array
+            timestamps.push(job.status.completionTime)
+        })
+        // sort should place the most recent timestamp at the last index of the array
+        // We cannot assume the timestamps will be sorted
+        timestamps.sort()
+        let mostRecentTimestamp = timestamps[timestamps.length - 1]
 
-        fetch('/kappnav/resource/commands?time=' + timeStampFromThirtySecondsAgo)
+        const store = window.localStorage
+        store.setItem(localStorage_key_last_seen_completed_job_timestamp, mostRecentTimestamp)
+    }
+
+    /**
+     * Get the current time from the Kubernetes platform where
+     * the jobs are running.
+     */
+    getPlatformCurrentTime() {
+        return fetch('/kappnav/resource/command-time')
             .then(result => result.json())
-            .then(result => this.processCompletedJobs(result))
+            .then(json => {
+                return json.time
+            })
+    }
+
+    /**
+     * Poll for completed jobs only that happened after a particular time
+     */
+    getCompletedJobs() {
+        this.getTimeStampOfLastSeenCompletedJob().then(timestamp => {
+            fetch('/kappnav/resource/commands?time=' + timestamp)
+                .then(result => result.json())
+                .then(result => this.processCompletedJobs(result))
+        })
     }
 
     /**
@@ -109,9 +160,13 @@ export default class NotificationHandler extends React.PureComponent {
      * @param {*} result - json response from API call
      */
     processCompletedJobs(result) {
-        this.setState({ completed_commands: result })
+        // The reason we are recording the most recent job completion time
+        // is to have the polling always ask for any jobs that completed after the
+        // last seen completed job.
+        this.setMostRecentJobCompletionTime(result)
         this.setLastAPICheckTime()
-    }
+        this.setState({ completed_commands: result })
+    } 
 
     /**
      * Helper method to do date calculation on whether the last timestamp
@@ -126,11 +181,11 @@ export default class NotificationHandler extends React.PureComponent {
             return true
         }
         last_time_polled = moment(last_time_polled) // convert to a moment object to do time malipulation
-        const next_polling_timestamp = last_time_polled.add('milliseconds', api_polling_intervals_milliseconds)
-        const current_timpstamp = moment()
+        const next_polling_timestamp = last_time_polled.add(api_polling_intervals_milliseconds, 'milliseconds')
+        const current_timestamp = moment()
 
         // current time is past next scheduled polling, POLL API!
-        return current_timpstamp.isAfter(next_polling_timestamp) 
+        return current_timestamp.isAfter(next_polling_timestamp) 
     }
 
     /**
@@ -138,6 +193,27 @@ export default class NotificationHandler extends React.PureComponent {
      * @param {*} numMillisecondsAgo - How far back from current time in milliseconds
      */
     getPastTimeStampFrom(numMillisecondsAgo) {
-        return moment().subtract('milliseconds', numMillisecondsAgo);
+        return moment().subtract(numMillisecondsAgo, 'milliseconds');
+    }
+
+    getTimeStampOfLastSeenCompletedJob() {
+        let timestamp = window.localStorage.getItem(localStorage_key_last_seen_completed_job_timestamp)
+        if(! timestamp) {
+            // fetch the current time of the platform.  We will assume this is the
+            // users first time using the UI.  The current time of the platform will
+            // be the starting timestamp we use to get the completd jobs last seen by
+            // the user
+            return this.getPlatformCurrentTime().then(timestamp => {
+                const store = window.localStorage
+                store.setItem(localStorage_key_last_seen_completed_job_timestamp, timestamp)
+                return timestamp
+            })
+        } else {
+            return Promise.resolve(timestamp)
+        }
     }
 }
+
+export default connect(
+    (state) => ({baseInfo: state.baseInfo})
+)(NotificationHandler)
